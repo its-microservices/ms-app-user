59af66a6d3e8eae257d9e73864e3c94bcfd982b9
Jeropa2 <huincamanjeronimo.dev@gmail.com>
2025-04-20 00:23:41 -0300

    feat: initialize NestJS application with user management module
    - Add package.json for project dependencies and scripts
    - Create AppController and AppService with a basic "Hello World!" endpoint
    - Implement User module with CRUD operations and pagination support
    - Set up TypeORM for MySQL database connection
    - Add environment variable configuration using dotenv and joi
    - Create DTOs for user creation and updates
    - Implement unit and e2e tests for controllers and services
    - Configure TypeScript settings for the project

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,64 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { PaginationDto } from 'src/common';
+
+@Injectable()
+export class UserService {
+
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) { }
+
+  async create(createUserDto: CreateUserDto) {
+    const user = this.userRepository.create(createUserDto);
+    return this.userRepository.save(user);
+  }
+
+  async findAll(PaginationDto: PaginationDto) {
+
+    const page = PaginationDto.page ?? 1;
+    const limit = PaginationDto.limit ?? 10;
+
+    const [data, total] = await this.userRepository.findAndCount({
+      where: { isActive: true },
+      order: { id: 'ASC' },
+      skip: (page - 1) * limit,
+      take: limit,
+    })
+
+    return { data, total, page, limit, totalPages: Math.ceil(total / limit) };
+  }
+
+  async findOne(id: number): Promise<User> {
+    const user = await this.userRepository.findOne({ where: { id } });
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+    return user;
+  }
+
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const { id: __, ...data } = updateUserDto;
+    const user = await this.findOne(id);
+
+    Object.assign(user, data);
+
+    return this.userRepository.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.userRepository.findOne({ where: { id } });
+
+    if (!user) {
+      throw new NotFoundException(`User with ID ${id} not found`);
+    }
+
+    await this.userRepository.softRemove(user);
+  }
+
+}
