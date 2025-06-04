201b3f80b57a9abdb1bc8b21d1daf11d155754d3
DiLucaLucas <lucasmartindiluca@gmail.com>
2025-06-04 10:15:45 -0300

    Feat: Implement CreateUserDto with validation rules

--- src/modules/user/dto/create-user.dto.ts
+++ src/modules/user/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+// src/user/dto/create-user.dto.ts
+import { IsString, IsOptional, IsBoolean } from 'class-validator';
+
+export class CreateUserDto {
+    @IsString()
+    firstName: string;
+
+    @IsString()
+    lastName: string;
+
+    @IsOptional()
+    @IsBoolean()
+    isActive?: boolean;
+}
